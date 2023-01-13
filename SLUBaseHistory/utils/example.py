9d51a9ba596fc2a40071cda55f816dcd25bb47dd
anguuan <anguuan@outlook.com>
2023-01-13 20:03:40 +0800

    move SLUBase with history

---
+++ SLUBaseHistory/utils/example.py
@@ -0,0 +1,67 @@
+import json
+
+from utils.vocab import Vocab, LabelVocab
+from utils.word2vec import Word2vecUtils
+from utils.evaluator import Evaluator
+
+
+class Example():
+
+    @classmethod
+    def configuration(cls, root, train_path=None, word2vec_path=None):
+        cls.evaluator = Evaluator()
+        cls.word_vocab = Vocab(padding=True, unk=True, filepath=train_path)
+        cls.word2vec = Word2vecUtils(word2vec_path)
+        cls.label_vocab = LabelVocab(root)
+
+    @classmethod
+    def load_dataset(cls, data_path):
+        dataset = json.load(open(data_path, 'r', encoding='utf-8'))
+        examples = []
+        for data in dataset:
+            conv = conversation()
+            for utt in data:
+                conv.app(cls(utt))
+            examples.append(conv)
+        return examples
+
+    def __init__(self, ex: dict):
+        self.utt = ex['asr_1best']
+        self.slot = {}
+        for label in ex['semantic']:
+            act_slot = f'{label[0]}-{label[1]}'
+            if len(label) == 3:
+                self.slot[act_slot] = label[2]
+        self.tags = ['O'] * len(self.utt)
+        for slot in self.slot:
+            value = self.slot[slot]
+            bidx = self.utt.find(value)
+            if bidx != -1:
+                self.tags[bidx: bidx + len(value)] = [f'I-{slot}'] * len(value)
+                self.tags[bidx] = f'B-{slot}'
+        self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]
+        self.input_idx = [Example.word_vocab[c] for c in self.utt]
+        l = Example.label_vocab
+        self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
+
+
+class conversation():
+    def __init__(self):
+        self.utt = ""
+        self.input_idx = []
+        self.tag_id = []
+        self.ex_lst = []
+        self.lengths_lst = []
+        self.labels = []
+
+    def app(self, ex):
+        if self.utt:
+            self.utt += ';'
+            self.input_idx.append(2)
+            self.tag_id.append(1)
+        self.utt += ex.utt
+        self.input_idx += ex.input_idx
+        self.tag_id += ex.tag_id
+        self.ex_lst.append(ex)
+        self.lengths_lst.append(len(ex.utt))
+        self.labels.append(ex.slotvalue)
---
+++ SLUBaseHistory/utils/word2vec.py
@@ -0,0 +1,37 @@
+#coding=utf8
+
+import numpy as np
+from utils.vocab import PAD, UNK, SEP
+import torch
+
+class Word2vecUtils():
+
+    def __init__(self, word2vec_file):
+        super(Word2vecUtils, self).__init__()
+        self.word2vec = {}
+        self.read_from_file(word2vec_file)
+
+    def load_embeddings(self, module, vocab, device='cpu'):
+        """ Initialize the embedding with glove and char embedding
+        """
+        emb_size = module.weight.data.size(-1)
+        outliers = 0
+        module.weight.data[vocab[PAD]] = torch.zeros(emb_size, dtype=torch.float, device=device)
+        module.weight.data[vocab[SEP]] = torch.zeros(emb_size, dtype=torch.float, device=device)
+        for word in vocab.word2id:
+            if word == PAD or word == SEP: # PAD symbol is always 0-vector
+                continue
+            word_emb = self.word2vec.get(word, self.word2vec[UNK])
+            module.weight.data[vocab[word]] = torch.tensor(word_emb, dtype=torch.float, device=device)
+        return 1 - outliers / float(len(vocab))
+
+    def read_from_file(self, word2vec_file):
+        with open(word2vec_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.strip('\n')
+                items = line.split(' ')
+                if len(items) <= 2:
+                    continue
+                word = items[0]
+                vector = np.fromstring(' '.join(items[1:]), dtype=float, sep=' ')
+                self.word2vec[word] = vector
