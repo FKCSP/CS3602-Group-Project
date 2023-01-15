22c02bb1af699679a8f7deaf8470cc7a208ef072
Yikai Zhang <1065543666@sjtu.edu.cn>
2023-01-15 02:18:10 +0000

    nothing to say

---
+++ a.py
@@ -0,0 +1,3 @@
+import numpy as np
+a = np.array([68.9548, 69.1320, 70.6612])
+print(a.std(), a.mean())
--- utils/args.py
+++ utils/args.py
@@ -14,16 +14,16 @@ def add_argument_base(arg_parser):
     #### General configuration ####
     arg_parser.add_argument('--dataroot', default='./data', help='root of data')
     arg_parser.add_argument('--word2vec_path', default='./word2vec-768.txt', help='path of word2vector file path')
-    arg_parser.add_argument('--seed', default=999, type=int, help='Random seed')
+    arg_parser.add_argument('--seed', default=99999, type=int, help='Random seed')
     arg_parser.add_argument('--device', type=int, default=7, help='Use which device: -1 -> cpu ; the index of gpu o.w.')
     arg_parser.add_argument('--testing', action='store_true', help='training or evaluation mode')
     #### Training Hyperparams ####
-    arg_parser.add_argument('--batch_size', default=16, type=int, help='Batch size')
+    arg_parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
     arg_parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
-    arg_parser.add_argument('--max_epoch', type=int, default=100, help='terminate after maximum epochs')
+    arg_parser.add_argument('--max_epoch', type=int, default=40, help='terminate after maximum epochs')
     #### Common Encoder Hyperparams ####
     arg_parser.add_argument('--encoder_cell', default='LSTM', choices=['LSTM', 'GRU', 'RNN'], help='root of data')
-    arg_parser.add_argument('--dropout', type=float, default=0.2, help='feature dropout rate')
+    arg_parser.add_argument('--dropout', type=float, default=0.1, help='feature dropout rate')
     arg_parser.add_argument('--embed_size', default=768, type=int, help='Size of word embeddings')
     arg_parser.add_argument('--hidden_size', default=512, type=int, help='hidden size')
     arg_parser.add_argument('--num_layer', default=2, type=int, help='number of layer')
